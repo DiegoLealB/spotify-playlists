8eddfa24c4072d4facabec29f554ef013f206259
diegolealb <diegolealb@outlook.com>
2019-03-08 16:40:21 -0500

    Finished basic user info component

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -2,15 +2,28 @@ import React from 'react';
 import Spotify from 'spotify-web-api-js';
 import { withStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
-
 
 const spotifyWebApi = new Spotify();
 
 const styles = {
     avatar: {
-        margin: 10,
+        position: 'absolute',
+        left: 0,
+        margin: '10px',
     },
+    profileContainer: {
+        width: '400px',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    link: {
+        textDecoration: 'none',
+        color: 'black',
+        position: 'absolute',
+        left: '60px',
+        margin: '0, 0, 10, 0'
+    }
 }
 
 class UserProfile extends React.Component {
@@ -23,17 +36,17 @@ class UserProfile extends React.Component {
 
     async getUser() {
         let user = await spotifyWebApi.getMe();
-        console.log('user', user)
         return user;
     }
 
     componentWillMount() {
-        let user;
         this.getUser().then(user => {
             this.setState({
                 user: {
                     name: user.display_name,
                     initial: user.display_name[0],
+                    link: user.external_urls.spotify,
+                    id: user.id,
                 },
             })
         })
@@ -43,14 +56,11 @@ class UserProfile extends React.Component {
         const { classes } = this.props;
 
         return (
-            <div>
-                <button onClick={() => this.getUser()}>
-                    Refresh
-                </button>
-                <h1>{this.state.user.name}</h1>
-                <Grid>
-                    <Avatar className={classes.avatar}>{this.state.user.initial}</Avatar>
-                </Grid>
+            <div className={classes.profileContainer}>
+                <Avatar className={classes.avatar}>{this.state.user.initial}</Avatar>
+                <a href={this.state.user.link} className={classes.link}>
+                    <h2>{this.state.user.name}</h2>
+                </a>
             </div>
         )
     }
