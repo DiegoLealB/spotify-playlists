14f1cf29d399d71c95efdd49918a9df1652c30fc
Diego <diegolealb@gmail.com>
2019-03-13 01:03:40 -0400

    App component now handles login

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,82 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import axios from 'axios';
+import Spotify from 'spotify-web-api-js';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import green from '@material-ui/core/colors/green';
 
 import HomePage from './containers/HomePage.container';
+import NavBar from './components/NavBar';
+import LoginButton from './components/LoginButton';
+
+const spotifyWebApi = new Spotify();
 
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
     primary: green,
-    secondary: { main: '#000000' },
-    background: { main: '#000000' }
   },
   typography: { useNextVariants: true },
   paper: {
-    backgroundColor: '#6d6d6d'
+    backgroundColor: '#191414'
   }
 })
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    let queryString = window.location.hash.substring(1);
+    const params = this.getHashParams(queryString);
+    
+    this.state = {
+        loggedIn: params.access_token ? true : false,
+        accessToken: params.access_token,
+        refreshToken: params.refresh_token,
+    }
+
+    if (params.access_token) {
+        spotifyWebApi.setAccessToken(params.access_token);
+    }
+  }
+
+  getHashParams(params) {
+      var hashParams = {};
+      var e, r = /([^&;=]+)=?([^&;]*)/g,
+          q = params
+      while ( e = r.exec(q)) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
+      }
+      return hashParams;
+  }
+
+  async componentDidMount() {
+    try {
+      // Refreshes access token every 30 minutes
+      const refreshToken = this.state.refreshToken;
+      setInterval(async () => {
+        const res = await axios.get(`http://localhost:8888/refresh_token?refresh_token=${refreshToken}`);
+        const accessToken = res.data.access_token;
+        spotifyWebApi.setAccessToken(accessToken);
+      }, 300000)
+    } catch(err) {
+      console.error('Refresh token error: ', err);
+    }
+  }
 
   render() {
+    const { loggedIn } = this.state;
+
     return (
-      <Router>
+      <BrowserRouter>
         <MuiThemeProvider theme={theme}>
-          <Route path="/" render={(props) => <HomePage {...props}/>} />
+        { loggedIn ?
+        <div>
+          <NavBar />
+          <Route exact path='/' component={HomePage} />
+        </div>
+        : <LoginButton /> }
         </MuiThemeProvider>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
--- client/src/containers/HomePage.container.js
+++ client/src/containers/HomePage.container.js
@@ -1,57 +1,16 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Spotify from 'spotify-web-api-js';
 
 // Component imports
-import LoginButton from '../components/LoginButton';
 import Playlists from '../components/Playlists';
-import NavBar from '../components/NavBar';
-
-const spotifyWebApi = new Spotify();
-
-const styles = {
-
-}
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-        let queryString = props.location.hash.substring(1);
-        
-        const params = this.getHashParams(queryString)
-        
-        this.state = {
-            loggedIn: params.access_token ? true : false,
-            token: params.access_token, 
-        }
-        if (params.access_token) {
-            spotifyWebApi.setAccessToken(params.access_token);
-        }
-    }
-
-    getHashParams(params) {
-        var hashParams = {};
-        var e, r = /([^&;=]+)=?([^&;]*)/g,
-            q = params
-        while ( e = r.exec(q)) {
-           hashParams[e[1]] = decodeURIComponent(e[2]);
-        }
-        return hashParams;
-    }
-
     render() {
-        const { loggedIn } = this.state;
         return (
-            <div className="App">
-                { loggedIn ?
-                    <div>
-                        <NavBar />
-                        <Playlists />
-                    </div>
-                : <LoginButton /> }
+            <div>
+                <Playlists />
             </div>
         )
     }
 }
 
-export default withStyles(styles)(HomePage);
+export default HomePage;
