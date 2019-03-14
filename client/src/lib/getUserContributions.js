f7df2e1e74f1436e9645de2201eaa69ca69d91ed
Diego <diegolealb@gmail.com>
2019-03-13 20:46:09 -0400

    Makes function asyncronous

--- client/src/lib/getUserContributions.js
+++ client/src/lib/getUserContributions.js
@@ -3,40 +3,59 @@ import Spotify from 'spotify-web-api-js';
 
 const spotifyWebApi = new Spotify();
 
-function getUserContribution(tracksArr) {
+// Takes in an array of tracks and returns data for a graph in chart.js format
+async function getUserContribution(tracksArr) {
+
+    // Returns a random integer from 0 to 255
+    function randomRGBValue() {
+        return Math.floor(Math.random() * 256);
+    }
+
     const userIds = tracksArr.map(track => {
         return track.added_by.id;
     })
 
+    // Gets count of occurrences of each user from the tracksArr
     const userCount = _.countBy(userIds)
     const users = Object.keys(userCount);
     const count = Object.values(userCount);
 
     let userNames = [];
-    users.forEach(user => {
-        spotifyWebApi.getUser(user)
-            .then(userInfo => {
-                userNames.push(userInfo.display_name)
-            })
-    })
+    let backgroundColorsArr = [];
+    let borderColorsArr = [];
 
-    const usersObj = {
-        title: 'Playlist contributions by user',
-        labels: userNames,
-        datasets: [{
-            label: '# of songs',
-            data: count,
-            backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-                'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1
-        }],
+    async function getUserInfo(user) {
+        return await spotifyWebApi.getUser(user)
     }
 
-    return usersObj;
+    // Gets all usernames and sets a background and border color for each user
+    for (let i = 0; i < users.length; i++) {
+        let userName = await getUserInfo(users[i]);
+        userNames.push(userName.display_name);
+
+        let backgroundColor = `rgba(${randomRGBValue()}, ${randomRGBValue()}, ${randomRGBValue()}, 0.2)`;
+        backgroundColorsArr.push(backgroundColor);
+
+        let borderColor = backgroundColor.replace('0.2', '1');
+        borderColorsArr.push(borderColor);
+    }
+    
+    async function setUsersObj() {
+        const usersObj = {
+            title: 'Playlist contributions by user',
+            labels: userNames,
+            datasets: [{
+                label: '# of songs',
+                data: count,
+                backgroundColor: backgroundColorsArr,
+                borderColor: borderColorsArr,
+                borderWidth: 1
+            }],
+        }
+        return usersObj;
+    }
+
+    return await setUsersObj();
 }
 
 export default getUserContribution;
