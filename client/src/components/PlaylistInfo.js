1e850f9d871f8aac5bddcce97b82dcb90dedbc62
Diego <diegolealb@gmail.com>
2019-03-13 20:46:34 -0400

    Makes into a class component to rerender based on state changes

--- client/src/components/PlaylistInfo.js
+++ client/src/components/PlaylistInfo.js
@@ -9,28 +9,49 @@ const styles = {
 
 }
 
-const PlaylistInfo = ({ playlist })  => {
-    const tracks = playlist.tracks.items
-    const timeData = getDatesByYear(tracks);
-    let userContributionData = {};
-    if (playlist.collaborative) {
-        userContributionData = getUserContributions(tracks);
+class PlaylistInfo extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            playlist: props.playlist,
+            userContributionData: undefined,
+        }
     }
     
-    return (
-        <div>
-            <h4> By: {playlist.owner.display_name}</h4>
-            {playlist.images[1] === undefined ?
-            playlist.images[0] === undefined ? <h1>No image</h1> 
-            : <img src={playlist.images[0].url} alt={playlist.name}></img>
-            : <img src={playlist.images[1].url} alt={playlist.name}></img> }
-            <br />
-            <Line data={timeData} />
-            { userContributionData.datasets ?
-                <Pie data={userContributionData} />
-            : null }
-        </div>
-    )
+    async componentWillMount() {
+        try {
+            const tracks = this.state.playlist.tracks.items;
+            if (this.state.playlist.collaborative && !this.state.userContributionData) {
+                const res = await getUserContributions(tracks)
+                this.setState({
+                    userContributionData: res,
+                })
+            }
+        } catch(err) {
+            console.error('getUserContributions error: ', err);
+        }
+    }
+    
+    render() {
+        const { playlist, userContributionData } = this.state;
+        const tracks = playlist.tracks.items
+        const timeData = getDatesByYear(tracks);
+        return (
+            <div>
+                <h4> By: {playlist.owner.display_name}</h4>
+                {playlist.images[1] === undefined ?
+                playlist.images[0] === undefined ? <h1>No image</h1> 
+                : <img src={playlist.images[0].url} alt={playlist.name}></img>
+                : <img src={playlist.images[1].url} alt={playlist.name}></img> }
+                <br />
+                <Line data={timeData} />
+                { userContributionData && playlist.collaborative === true ?
+                    <Pie data={userContributionData} />
+                : null }
+            </div>
+        )
+    }
 }
+
 
 export default withStyles(styles)(PlaylistInfo)
