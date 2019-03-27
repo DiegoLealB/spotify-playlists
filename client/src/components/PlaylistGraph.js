45da6482f8b709e3764e11fe68dfcdd51958446f
Diego <diegolealb@gmail.com>
2019-03-26 20:18:11 -0400

    Adds new graphTypes and a random starting graph

---
+++ client/src/components/PlaylistGraph.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Bar, Bubble, Doughnut, Line, Radar, Pie, Polar } from 'react-chartjs-2';
+import { withStyles } from '@material-ui/core/styles';
+import { InputLabel, Select, MenuItem } from '@material-ui/core';
+
+const styles = {
+
+}
+
+class PlaylistGraphs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            graph: '',
+        }
+    }
+
+    handleChange = event => {
+        this.setState({ graph: event.target.value });
+    };
+
+    getRandomGraph() {
+        const graphTypes = ['Bar', 'Line', 'Polar', 'Pie', 'Dougnut', 'Bubble'];
+        const randomNum = Math.floor(Math.random() * graphTypes.length);
+        return graphTypes[randomNum];
+    }
+
+    componentWillMount() {
+        this.setState({
+            graph: this.getRandomGraph(),
+        })
+    }
+    
+    render() {
+        const { classes } = this.props;
+
+        const playlistData = this.props.children;
+        let options;
+        if (playlistData.options) {
+            options = playlistData.options;
+        }
+
+        return (
+            <div>
+                <InputLabel htmlFor="customized-select">Graph type</InputLabel>
+                <Select
+                    value={ this.state.graph }
+                    onChange={ this.handleChange }
+                    id="customized-select"
+                >
+                    <MenuItem value='Bar'>Bar</MenuItem>
+                    <MenuItem value='Bubble'>Bubble</MenuItem>
+                    <MenuItem value='Doughnut'>Doughnut</MenuItem>
+                    <MenuItem value='Line'>Line</MenuItem>
+                    <MenuItem value='Pie'>Pie</MenuItem>
+                    <MenuItem value='Radar'>Radar</MenuItem>
+                    <MenuItem value='Polar'>Polar</MenuItem>
+                </Select>
+                { this.state.graph === 'Bar' ? <Bar data={ playlistData } options={ options }/>
+                : this.state.graph === 'Bubble' ? <Bubble data={ playlistData } options={ options }/>
+                : this.state.graph === 'Doughnut' ? <Doughnut data={ playlistData } options={ options }/>
+                : this.state.graph === 'Line' ? <Line data={ playlistData } options={ options }/>
+                : this.state.graph === 'Pie' ? <Pie data={ playlistData } options={ options }/>
+                : this.state.graph === 'Radar' ? <Radar data={ playlistData } options={ options }/>
+                : <Polar data={ playlistData } options={ options }/> }
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(PlaylistGraphs);
