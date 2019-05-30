a11369a3fd5a544b02df1c68d976affa7cbe29e5
Diego Ignacio Leal <diegolealb@outlook.com>
2019-05-30 18:37:20 -0400

    Starts on release date helper function

---
+++ client/src/lib/getReleaseDate.js
@@ -0,0 +1,68 @@
+function getReleaseDate(tracksArr) {
+    const dates = tracksArr.map(track => {
+        return new Date(track.track.album.release_date);
+    });
+
+    let releaseDatesByYear = dates.map(date => {
+        return date.toString().split('-')[0];
+    });
+
+    releaseDatesByYear = releaseDatesByYear.sort();
+
+    const startYear = releaseDatesByYear[0];
+    const endYear = releaseDatesByYear[releaseDatesByYear.length - 1];
+    const diff = endYear - startYear;
+    let yearsArr = [];
+
+    for (let i = 0; i < diff + 1; i++) {
+        yearsArr.push(Number(startYear) + i);
+    }
+    
+    const namesArr = tracksArr.map(track => {
+        return track.track.name;
+    });
+
+    const releaseDatesObj = {
+        labels: namesArr,
+        datasets: [{
+            label: 'Tracks by release date',
+            data: [{
+                x: dates,
+                y: releaseDatesByYear,
+            }],
+        }],
+        options: {
+            title: {
+                display: true,
+                fontSize: 20,
+                text: 'Tracks release date',
+            },
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    distribution: 'linear',
+                    time: {
+                        displayFormats: {
+                            week: 'll',
+                        }
+                    },
+                }],
+                yAxes: [{
+                    type: 'linear',
+                    position: 'left',
+                    ticks: {
+                        min: yearsArr[0],
+                        max: yearsArr[yearsArr.length - 1],
+                    }
+                }]
+            }
+        }
+    }
+
+    return releaseDatesObj;
+}
+
+export default getReleaseDate;
